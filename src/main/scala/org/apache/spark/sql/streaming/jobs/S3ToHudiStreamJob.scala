f77bced44f809ee2156a273779932f62fa447950
Bhupender Panwar <bpanwar@salesforce.com>
2022-06-24 15:55:17 -0700

    first big commit working with S3 normalizer

--- src/main/scala/org/apache/spark/sql/streaming/jobs/S3ToHudiStreamJob.scala
+++ src/main/scala/org/apache/spark/sql/streaming/jobs/S3ToHudiStreamJob.scala
@@ -37,6 +37,7 @@ object S3ToHudiStreamJob extends Logging {
       .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
       .config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com")
       .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+      .config("spark.dynamicAllocation.enabled", "false")
       .getOrCreate()
     spark.sparkContext.setLogLevel(Config().getString("normv2.loggerLevel"))
     spark.sqlContext.udf.register("uuid", uuid)
