7b6634aa65a90650f409fc39237d13487001841d
Bhupender Panwar <bpanwar@salesforce.com>
2023-01-05 18:07:34 -0800

    cloud trail and string transform

--- src/main/scala/org/apache/spark/sql/streaming/jobs/StringTransformJob.scala
+++ src/main/scala/org/apache/spark/sql/streaming/jobs/StringTransformJob.scala
@@ -31,12 +31,15 @@ object StringTransformJob {
     import spark.sqlContext.implicits._
     val path = "/Users/bpanwar/Desktop/file1.txt"
     //val jsonString1 = """{"Zipcode":704,"ZipCodeType":"STANDARD1","City":"PARC PARQUE1","State":"PR1"}"""
-   // val jsonString2 = """{"Zipcode":705,"ZipCodeType":"STANDARD2","City":"PARC PARQUE2","State":"PR2"}"""
+     // val jsonString2 = """{"Zipcode":705,"ZipCodeType":"STANDARD2","City":"PARC PARQUE2","State":"PR2"}"""
     //val data = Seq((1, jsonString1)).add(2, jsonString2)
-   val df = spark.read.text(path)
+    val df = spark.read.textFile(path)
     df.show(false)
+    val df6 = spark.read.option("mergeSchema", true).json(df)
+    df6.printSchema()
+    df6.show(false)
 
-    import org.apache.spark.sql.functions.{from_json, col}
+   /* import org.apache.spark.sql.functions.{from_json, col}
     import org.apache.spark.sql.types.{MapType, StringType}
     val df2 = df.withColumn("value", from_json(col("value"), MapType(StringType, StringType)))
     df2.printSchema()
@@ -46,7 +49,7 @@ object StringTransformJob {
     val json_schema = spark.read.json(df2.select(json_col).as[String]).schema
     val df3 = df2.withColumn("value", from_json(json_col, json_schema))
     val df4 = df3.select("*", "value.*").drop("value")
-    df4.show()
+    df4.show()*/
   }
 
   def uuid: UserDefinedFunction = udf(() => java.util.UUID.randomUUID().toString)
