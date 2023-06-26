63da9be214a5861e0a5c9101dfee08dd31e0c0eb
Bhupender Panwar <bpanwar@salesforce.com>
2023-06-26 12:53:33 -0700

    changes sns sqs

--- src/main/java/org/apache/spark/sql/streaming/sqs/BasicSessionCredentialsProvider.java
+++ src/main/java/org/apache/spark/sql/streaming/sqs/BasicSessionCredentialsProvider.java
@@ -2,25 +2,28 @@ package org.apache.spark.sql.streaming.sqs;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicSessionCredentials;
 import org.apache.commons.lang.StringUtils;
 
-public class BasicAWSCredentialsProvider implements AWSCredentialsProvider {
+public class BasicSessionCredentialsProvider implements AWSCredentialsProvider {
   private final String accessKey;
   private final String secretKey;
+  private final String sessionToken;
 
-  public BasicAWSCredentialsProvider(String accessKey, String secretKey) {
+  public BasicSessionCredentialsProvider(String accessKey, String secretKey, String sessionToken) {
     this.accessKey = accessKey;
     this.secretKey = secretKey;
+    this.sessionToken = sessionToken;
   }
 
   public AWSCredentials getCredentials() {
-    if (!StringUtils.isEmpty(accessKey) && !StringUtils.isEmpty(secretKey)) {
-      return new BasicAWSCredentials(accessKey, secretKey);
+    if (!StringUtils.isEmpty(accessKey)
+            && !StringUtils.isEmpty(secretKey) && !StringUtils.isEmpty(sessionToken)) {
+      return new BasicSessionCredentials(accessKey, secretKey, sessionToken);
     }
     throw new AmazonClientException(
-        "Access key or secret key is null");
+            "Access key or secret key is null");
   }
 
   public void refresh() {}
